3e5f8d37338b815b80a6b3379e5e9fe6c4a09061
Reel <ladias231299@gmail.com>
2024-04-14 20:01:03 +0300

    feat: add solution for getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -238,8 +238,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let nextId = startFrom - 1;
+
+  return () => {
+    nextId += 1;
+    return nextId;
+  };
 }
 
 module.exports = {
